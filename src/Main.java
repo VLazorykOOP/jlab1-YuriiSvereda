7b5c2646b8c9878ac1b249e534f55f20b6fb4fe0
SveredaYurii <svereda.yurii@chnu.edu.ua>
2023-09-25 23:54:31 +0300

    created start menu. fully working version

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Task4.test();
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("choose task:");
+            System.out.println("1. Task1;\n2. Task2;\n3. Task3;\n4. Task4.");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    Task1.test();
+                    break;
+                case 2:
+                    Task2.test();
+                    break;
+                case 3:
+                    Task3.test();
+                    break;
+                case 4:
+                    Task4.test();
+                    break;
+                case -1:
+                    exit = true;
+                default:
+                    System.out.println("there is no such option :(");
+                    break;
+            }
+            System.out.println("========================================================================================\n\n\n\n\n");
+        }
     }
 }
